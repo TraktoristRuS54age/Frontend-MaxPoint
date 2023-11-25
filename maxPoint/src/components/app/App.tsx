d35938d2decf0d29e067c27fac412ed7dac9b737
justproggramer <morozow2121@mail.ru>
2023-11-26 02:39:03 +0300

    some change

--- maxPoint/src/components/app/App.tsx
+++ maxPoint/src/components/app/App.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable sort-imports */
-import "./App.css";
 import Header from "../Header/Header.tsx";
 import Editor from "../Editor/Editor.tsx";
 import { Presentation } from "../../types/types.ts";
