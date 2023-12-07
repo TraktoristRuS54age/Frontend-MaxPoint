2524063b9043fa8638e0d31641d73a6865c49d60
justproggramer <morozow2121@mail.ru>
2023-12-07 21:15:28 +0300

    change before update

---
+++ maxPoint/src/components/store/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable sort-keys */
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export default store;
