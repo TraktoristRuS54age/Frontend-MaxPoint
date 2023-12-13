4a730699539b6728768750120d0f98f632aaff07
Wardus23 <124914288+Wardus23@users.noreply.github.com>
2023-12-13 23:23:07 +0300

    Добавил выпадающее меню для Figures (#16)
    однако нужно доработать стили

--- maxPoint/src/components/LeftToolBar/LeftToolBar.tsx
+++ maxPoint/src/components/LeftToolBar/LeftToolBar.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable sort-imports */
+import { useState } from "react";
 import text from "../../resources/img/text.png";
 import figures from "../../resources/img/figures.png";
 import photo from "../../resources/img/photo.png";
 import style from "./LeftToolbar.module.css";
 import UploadButton from "../UploadButton/UploadButton";
+import classNames from "classnames";
 
 function Left_ToolBar() {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className={style.menu_block}>
       <div className={style.menu_block__list}>
@@ -15,8 +18,33 @@ function Left_ToolBar() {
         </div>
 
         <div className={style.menu_item}>
-          <img className={style.menu_block__img} src={figures}></img>
-          <p className={style.menu_block_text}>Figure</p>
+          <button
+            className={style.figure__button}
+            onClick={() => setOpen(!isOpen)}>
+            <img className={style.menu_block__img} src={figures}></img>
+            <p className={style.menu_block_text}>Figure</p>
+          </button>
+          <nav 
+            className={
+              isOpen ? style.figure + " " + style.active : style.figure}>
+            <ul className={style.figure__menu}>
+              <li className={style.figure__item}>
+                <button className={style.figure__button}>
+                  <p>Треугольник</p>
+                </button>
+              </li>
+              <li className={style.figure__item}>
+                <button className={style.figure__button}>
+                  <p>Прямоугольник</p>
+                </button>
+              </li>
+              <li className={style.figure__item}>
+                <button className={style.figure__button}>
+                  <p>Круг</p>
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
 
         <div className={style.menu_item}>
