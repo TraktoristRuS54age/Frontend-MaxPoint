15adb7645fd22fcdc0426f118b271c2260db1bda
justproggramer <morozow2121@mail.ru>
2024-01-09 15:05:00 +0300

    del context
    context?

--- maxPoint/src/main.tsx
+++ maxPoint/src/main.tsx
@@ -1,6 +1,5 @@
 import "./index.css";
 import App from "./components/app/App.tsx";
-import PresentationProvider from "./context/context.tsx";
 import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom/client";
